439242c99debdce9e3c49dc0f1fe1a6d1e5cef14
WebluxDigi <team.webluxdigi@gmail.com>
2024-04-28 14:39:58 +0530

    Update chat.php

--- Public/Pages/Chat/app/helpers/chat.php
+++ Public/Pages/Chat/app/helpers/chat.php
@@ -18,7 +18,7 @@ function getChats($id_1, $id_2, $conn)
                 LEFT JOIN user AS sender ON chats.from_id = sender.id
                 LEFT JOIN user AS receiver ON chats.to_id = receiver.id
                 WHERE (chats.from_id = ? OR chats.to_id = ?)
-                ORDER BY chats.chat_id DESC";
+                ORDER BY chats.chat_id ASC";
         $params = [$id_1, $id_1];
 
     // } else {
