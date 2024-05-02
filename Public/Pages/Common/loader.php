9a1238250d41cdd5dd54b09d94f10a80fc7d321f
Jayesh9819 <dubeyjayesh345@gmail.com>
2024-05-02 19:04:15 +0530

    loader
    ererhie

--- Public/Pages/Common/loader.php
+++ Public/Pages/Common/loader.php
@@ -2,6 +2,8 @@
     <div class="loader simple-loader">
         <div class="loader-body ">
             <img src="<?php echo $settings['loader']; ?>" alt="loader" style="height: 25%;" class="image-loader img-fluid ">
+            <img src="./uploads/loader-bg.mp4" alt="New Image Description" style="width: 100%; height: 100%; object-fit: cover; position: absolute;" class="new-image img-fluid">
+
         </div>
     </div>
 </div>
