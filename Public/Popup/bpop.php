2d163f4075c98778c5401896ab2776b59282e62a
Jayesh9819 <dubeyjayesh345@gmail.com>
2024-05-05 04:56:16 +0530

    Update bpop.php

--- Public/Popup/bpop.php
+++ Public/Popup/bpop.php
@@ -3,9 +3,8 @@
 header('Content-Type: text/event-stream');
 header('Cache-Control: no-cache');
 header('Access-Control-Allow-Origin: *'); // Enable CORS if needed
-
 // Database connection
-include './App/db/db_connect.php';
+include '../../App/db/db_connect.php';
 
 // Debugging: Log that the script has started
 error_log("Started SSE script");
