d00c2e27ed11e17f06ac1bcfc98554f3e1be2c78
Jayesh9819 <dubeyjayesh345@gmail.com>
2024-05-05 04:42:02 +0530

    Update popup.php

--- Public/Popup/popup.php
+++ Public/Popup/popup.php
@@ -35,7 +35,7 @@
 
     // Create EventSource to listen for notifications
     console.log("Setting up EventSource for notifications."); // Debugging
-    let eventSource = new EventSource('../Public/Popup/popup.php');
+    let eventSource = new EventSource('../Public/Popup/bopup.php');
 
     // Event listener for receiving notifications
     eventSource.onmessage = function(event) {
