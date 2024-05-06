cbd4a21fd5272c9b19e48fa53831e89b719e42e9
Jayesh9819 <dubeyjayesh345@gmail.com>
2024-05-06 12:24:53 +0530

    Update index.php

--- Public/Pages/Chat/index.php
+++ Public/Pages/Chat/index.php
@@ -367,9 +367,6 @@
 
 			<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 			<script>
-				setInterval(function() {
-					location.reload();
-				}, 10000);
 
 				function fetchConversations() {
 					$.ajax({
