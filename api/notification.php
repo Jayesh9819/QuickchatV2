5aa4e19bc5ce047eb01d7a921338b83d4cbc6c28
Jayesh9819 <dubeyjayesh345@gmail.com>
2024-05-13 04:37:32 +0530

    Update notification.php

--- api/notification.php
+++ api/notification.php
@@ -8,7 +8,7 @@ FROM chats
 JOIN user ON chats.from_id = user.id 
 WHERE chats.opened = 0 
 AND chats.from_id = $userid 
-AND chats.created_at >= NOW() - INTERVAL 2 SECOND;";
+AND chats.created_at >= NOW();";
 if ($result = $conn->query($sql)) {
     if ($result->num_rows > 0) {
         while ($row = $result->fetch_assoc()) {
