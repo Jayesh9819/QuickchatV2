439b50e9074772e91facdfa133bf7d059122fb8f
Jayesh9819 <dubeyjayesh345@gmail.com>
2024-06-24 16:53:02 +0530

    Update sidebar.php

--- Public/Pages/Common/sidebar.php
+++ Public/Pages/Common/sidebar.php
@@ -210,7 +210,8 @@
                             <svg fill="none" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
     <path d="M19.75 11.7256L4.75 11.7256" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
     <path d="M13.7002 5.70124L19.7502 11.7252L13.7002 17.7502" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-  </svg>                        </a>
+  </svg> </i>
+                        </a>
                         <ul class="sub-nav collapse" id="sidebar-special" data-bs-parent="#sidebar-menu">
                             <?php if (in_array($role, ['Supervisor', 'Manager', 'Admin', 'Agent', 'Users'])) : ?>
 
