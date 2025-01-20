cf84335129b27ab21aafdd86e9e9bd2b7376736d
Jayesh9819 <dubeyjayesh345@gmail.com>
2025-01-20 12:51:04 +0530

    Auto commit

--- App/db/db_connect.php
+++ App/db/db_connect.php
@@ -9,7 +9,7 @@ $dbname = "quickchat";
 // Create connectionhh
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
+// Check connectionh
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } else {
